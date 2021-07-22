ed84cf7eee743de240f3a779ffe71f6a4ae75c8c
Matt Austin <matt.austin@englishcentral.com>
2021-07-22 14:42:03 -0400

    Updated the Product name again to avoid collisions.

--- Example/PopupViewController.swift
+++ Example/PopupViewController.swift
@@ -1,5 +1,5 @@
 import UIKit
-import ECSwipeMenuViewController
+import ENGCENSwipeMenuViewController
 
 class PopupViewController: UIViewController {
 
--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -1,5 +1,5 @@
 import UIKit
-import ECSwipeMenuViewController
+import ENGCENSwipeMenuViewController
 
 final class ViewController: SwipeMenuViewController {
 
